a3e8b534885789e1509e2901e41e38c057bb5659
fron-tn <frontn365@gmail.com>
2025-09-23 13:09:17 +0200

    Add files via upload

---
+++ Book 2/Chapter 2/chapter2.js
@@ -0,0 +1,26 @@
+alert ("Life is good!");
+
+
+for (var i = 0; i < 30; i++) {
+    document.write("Life is interesting");
+};
+
+var element = document.createElement('div');
+element.textContent = "Testing123!";
+document.body.append(element);
+
+for (var i = 0; i < 15; i++) { 
+    document.write ("Joe said,\"Hi!\""); 
+};
+
+for (var j = 0; j < 5; j++) { 
+    document.write (' Johnson said,"Hi!" '); 
+};
+
+for (var i = 0; i < 3; i++) { 
+    document.write ("Weyrey said,'Hi' "); 
+};
+
+for (var i=0;i<10;i++){
+    document.write("Saburi said, 'Hi!' ")
+};
