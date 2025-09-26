747f49ee4c42c0cfc285450cf8fc5a2ab7fb1a4a
fron-tn <frontn365@gmail.com>
2025-09-26 09:39:30 +0200

    Add files via upload

---
+++ Book 2/Chapter 2/Chapter 3 - Copy/chapter3.js
@@ -0,0 +1,103 @@
+// Creating a Variable
+
+// var book;
+
+// var myFirstName;
+// var favoriteFood
+// var birthday;
+// var timeOfDay;
+
+// var _petName;
+// var $schoolSubject;
+
+// var friends_Age;
+// var street$address;
+
+// Results in error (reserved keywords)
+
+// var break = "Tomorrow";
+// var switch = "Today";
+
+// Storing Data in Variables
+
+// var book = "JaveScript For Kids For Dummies";
+// console.log(book);
+
+// book = "The Habit Of Living A Truth";
+// console.log(book);
+
+
+// Understanding Data Types
+
+// book = "The Habit Of Living A Truth";
+// console.log(book.length);
+// console.log("The Habit Of Living A Truth".length);
+
+// var string = "I am a string".indexOf("am");
+// console.log(string);
+
+// var string1 = "I am a string.".indexOf("I");
+// console.log(string1);
+
+// var numberTest = "10" + 10;
+// console.log(numberTest);
+
+// var numberTest1 = 10 / "10";
+// console.log(numberTest1);
+
+// var numberTest2 = 5 * "10";
+// console.log(numberTest2);
+
+// var numberTest3 = "10" * 10;
+// console.log(numberTest3);
+
+// console.log(1 < 10);
+// console.log(2 === 2);
+// console.log(1 >= 5);
+
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+
+// Prompting For User Input
+
+// var myName = prompt ("What is your name?");
+// console.log(myName);
+
+// alert("Good job!");
+// alert(true);
+
+
+// var myName = "Samuel Maybach";
+// var myAge = 50;
+// alert("Hi my name is " + myName + " My current age is " + myAge);
+
+
+// var truck = {
+//     "color": ["Yellow", "Red"],
+// price: 150000,
+// "drive" : function () {
+//     console.log("I am driving a big truck");
+// }
+// };
+
+// console.log(truck.color);
+// console.log(truck["color"]);
+// truck.drive();
+
+// document.writeln("Hi, Mom!");
+// document.writeln(333 + 100);
+
+// document.writeln("<h1>What are you doing right now?</h1>");
+// document.writeln("<p>I am coding sir.</p>");
+
+
+// Combining Input & Output
+
+// var toName = prompt("Please enter a recipient name:");
+// var fromName = prompt("Please enter sender's name");
+// var letterBody = prompt("Compose a letter");
+
+// document.writeln("Dear " + toName + ", <br><br>");
+// document.writeln(letterBody + "<br><br>");
+// document.writeln("Sincerely, <br>");
+// document.writeln(fromName);
